4a6034c230374cfaa084936fa42dfe67c9723623
KavyaMajety <48449567+KavyaMajety@users.noreply.github.com>
2020-03-25 10:04:41 +0530

    my commit

---
+++ JUnitTdd/src/main/java/epamht8/JUnitTdd/App.java
@@ -0,0 +1,13 @@
+package epamht8.JUnitTdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
